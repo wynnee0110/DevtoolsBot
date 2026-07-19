a93875ebf1d558679fd9b1059cccafe2adca0e21
Wayne <wynnee0110@gmail.com>
2026-07-19 20:08:09 +0800

    upd

--- app/handlers/ping.py
+++ app/handlers/ping.py
@@ -2,7 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
-from services.ping import ping_website
+from app.services.ping import ping_website
 
 router = Router()
 
